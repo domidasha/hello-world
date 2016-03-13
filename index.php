8c5d7e09da41979e47cc4288440b4b68e0d6cf49
dashadomi <daria.stukal@gmail.com>
2016-03-13 16:44:28 +0300

    Add picture upload

--- index.php
+++ index.php
@@ -1,25 +1,52 @@
 <!DOCTYPE>
 <html>
 <head>
-	<meta charset="unt-8">
 	<title>Form</title>
+
+	<style type="text/css">	
+	img{
+		width: 200px;
+		border: 1px solid #000;
+	}
+	</style>
+
 </head>
 <body>
-	<?php
 
+	<?php
+	
 	if ($_SERVER['REQUEST_METHOD']=='POST') {
+
+		//file destination	
+		$uploaddir = './uploads/';
+		$uploadfile = $uploaddir.basename($_FILES['uploadfile']['name']);
+
+		// copy:
+		if (copy($_FILES['uploadfile']['tmp_name'], $uploadfile)){
+			echo "<h3>all right</h3>";
+		}
+		else {
+			echo "<h3>Error</h3>"; exit; 
+		}
+
+		//picture
+		echo "<h3>Your picture: </h3>";
+		echo "<img src='".$uploadfile ."'>";
+
+		// info 
+		echo "<h3>Info: </h3>";
 		print_r($_FILES);
 	}
+
 	if ($_SERVER['REQUEST_METHOD']=='GET') {
 		?>
-		<form action="<?php $SERVER['PHPSELF']; ?>" method="POST" enctype="multipart/form-data">
-			Press Button: <br>
-			<input type="file" name="pic">
-			<input type="submit"	value="Submit">
-		</form>
-		<?php
+	<form action="index.php" method="post" enctype="multipart/form-data">
+	    Select image to upload:
+		<input type="file" name="uploadfile">
+		<input type="submit" value="Submit">
+	</form>
+	<?php
 	}
-
 	?>
 </body>
 </html>

01b75d6116e4e102d43f1efb1e53b0cb8d465bba
dasha <daria.stukal@gmail.com>
2016-03-15 13:49:10 +0300

    update database

--- database.sql
+++ database.sql
@@ -1,1 +1,16 @@
- 
+create table user (
+id int NOT NULL AUTO_INCREMENT,
+name varchar(100) NOT NULL,
+password varchar(100) NOT NULL,
+create_at TIMESTAMP,
+PRIMARY KEY (id)
+); 
+
+create table twitts (
+id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_id int,
+message varchar(100),
+image_path varchar(140) DEFAULT NULL,
+create_at TIMESTAMP
+); 
+
